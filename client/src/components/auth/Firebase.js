08108e3ed27949660bb703b9b170676bd0c3593a
marwen <marwen.mrabti.me@gmail.com>
2022-07-23 10:59:00 +0100

    remove firebase personal data

--- client/src/components/auth/Firebase.js
+++ client/src/components/auth/Firebase.js
@@ -7,12 +7,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyD7MdypSydUD1mIMe85GkkpbSCJxVajNn0',
-  authDomain: 'memories-c174e.firebaseapp.com',
-  projectId: 'memories-c174e',
-  storageBucket: 'memories-c174e.appspot.com',
-  messagingSenderId: '614030336656',
-  appId: '1:614030336656:web:98fc23158fad7b364eae15',
+ your firebase app info
 };
 
 // Initialize Firebase
